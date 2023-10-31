a3fdf76e74864e4dc0f4e07af91f4696b3e9db77
Viraj <viraj.sonawane22@pccoepune.org>
2023-10-31 20:50:03 +0530

    day 3 completed

--- src/customer/components/HomeSectionCarosel/HomeSectionCarosel.jsx
+++ src/customer/components/HomeSectionCarosel/HomeSectionCarosel.jsx
@@ -1,34 +1,72 @@
-import React from 'react';
+import React, { useState } from 'react';
 import AliceCarousel from 'react-alice-carousel';
-import HomeSectionCard from '../HomeSectionCard/HomeSectionCard'; // Make sure to adjust the import path based on your project structure.
+import HomeSectionCard from '../HomeSectionCard/HomeSectionCard';
+import KeyboardArrowLeftIcon from '@mui/icons-material/KeyboardArrowLeft';
+import { Button } from '@mui/material';
 
-const HomeSectionCarosel = () => {
+const HomeSectionCarousel = () => {
+  const [activeIndex, setActiveIndex] = useState(0);
   const responsive = {
     0: { items: 1 },
     720: { items: 2 },
-    1024: { items: 4 },
+    1024: { items: 5.5 },
   };
 
-  const items = [
-    <HomeSectionCard />,
-    <HomeSectionCard />,
-    <HomeSectionCard />,
-    <HomeSectionCard />,
-    <HomeSectionCard />,
-  ];
+  const slidePrev = () => {setActiveIndex(activeIndex - 1), console.log(items)};
+
+  const slideNext = () => {setActiveIndex(activeIndex + 1), console.log(items)};
+
+  const syncActiveIndex = ({ item }) => setActiveIndex(item);
+
+  const items = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,].map((item) => <HomeSectionCard/>)
+
 
   return (
-    <div>
-      <AliceCarousel
-        items={items}
-        disableButtonsControls
-        autoPlay
-        autoPlayInterval={1000}
-        infinite
-        responsive={responsive}
-      />
+    <div className='px-4 lg:px-8'>
+      <div className='border' style={{ position: 'relative' }}>
+        <AliceCarousel
+          items={items}
+          disableButtonsControls
+          infinite
+          responsive={responsive}
+          disableDotsControls
+          onSlideChange={syncActiveIndex}
+          activeIndex={activeIndex}
+        />
+        <Button
+          variant="contained"
+          onClick={slideNext}
+          className='z-50 bg-white' 
+          sx={{
+            position: 'absolute',
+            top: '8rem',
+            right: '0rem',
+            transform: 'translateX(50%) rotate(90deg)',
+            bgcolor: 'white',
+          }}
+          aria-label="next"
+        > 
+          <KeyboardArrowLeftIcon sx={{ transform: 'rotate(90deg)', color: 'black' }} />
+        </Button>
+
+        <Button
+          variant="contained"
+          onClick={slidePrev}
+          className='z-50 bg-white' 
+          sx={{
+            position: 'absolute',
+            top: '8rem',
+            left: '0rem',
+            transform: 'translateX(50%) rotate(-90deg)',
+            bgcolor: 'white',
+          }}
+          aria-label="next"
+        > 
+          <KeyboardArrowLeftIcon sx={{ transform: 'rotate(90deg)', color: 'black' }} />
+        </Button>
+      </div>
     </div>
   );
 };
 
-export default HomeSectionCarosel;
+export default HomeSectionCarousel;
--- src/customer/pages/HomePage.jsx
+++ src/customer/pages/HomePage.jsx
@@ -6,7 +6,12 @@ const HomePage = () => {
   return (
     <div>
       <MainCarosel/>
-      <div>
+      <div className='space-y-10 py-20 flex flex-col justify-center px-5 lg:px-10'>
+        <HomeSectionCarosel/>
+        <HomeSectionCarosel/>
+        <HomeSectionCarosel/>
+        <HomeSectionCarosel/>
+        <HomeSectionCarosel/>
         <HomeSectionCarosel/>
     </div>
     </div>
